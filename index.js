a49aa92e6eefe77b80d1f3580e689fbe10ec8b25
Zai Santillan (Palskie) <zaisantillan14@gmail.com>
2022-07-06 17:21:59 +0800

    feat: use @erutidians/auto-dop

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-import fs from 'fs';
-import { pad, open, diff, today, template, success, error } from './helpers.js';
+import { run } from '@erutidians/auto-dop';
 
-console.log(today);
+const user = {
+  name: 'Zai Santillan',
+  github: 'plskz',
+  start: '6/1/2022',
+  round: 1,
+};
 
-try {
-  const path = `Round-1/day${pad(diff)}.md`;
-
-  if (fs.existsSync(path)) {
-    // for safety reasons, to avoid overwriting existing files
-    error(`${path} already exists.`);
-    error(`delete if you want to override it`);
-    open(path);
-  } else {
-    fs.writeFileSync(path, template);
-    success(`${path} created successfully`);
-    open(path);
-  }
-} catch (err) {
-  console.error(err);
-}
+run(user)
